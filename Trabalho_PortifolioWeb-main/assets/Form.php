17a43c84cb22f255da8805a5ee3739afcf2ee53d
mauroyaga <99704564+mauroyaga@users.noreply.github.com>
2022-11-30 21:17:11 -0300

    Add files via upload

---
+++ Trabalho_PortifolioWeb-main/assets/Form.php
@@ -0,0 +1,32 @@
+<?php
+$nome = $_POST['nome'];
+  $email = $_POST['Email'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "mauro.yaga@outlook.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=utf-8\n";
+  $headers .= "From: $nome <$email>\n";
+ 
+
+  //Enviar
+  ini_set mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+?>
